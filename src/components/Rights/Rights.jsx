9df5c22838948dde101f045635cbbf20a3e40069
Vladimir661 <xphigota@gmail.com>
2024-12-06 23:29:54 +0200

    Main

--- src/components/Rights/Rights.jsx
+++ src/components/Rights/Rights.jsx
@@ -4,7 +4,7 @@ import styles from './Rights.module.css'
 function Rights() {
     return (
         <div className={styles.rights}>
-            <span> &copy; 2024 Delloq - All Rights Reserved </span>
+            <span> &copy; 2024 Dwelloq - All Rights Reserved </span>
         </div>
     );
 }
